350e67d7656c3202ec839d55d89dfd3b0b7ade0b
Jan Rabe <jan.rabe@exozet.com>
2018-08-01 15:23:01 +0200

    added onItemclicklistener

---
+++ Xamarin.Android.RecyclerView.Presenter/net/kibotu/recyclerview/OnItemClickListener.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Android.Views;
+
+namespace net.kibotu.recyclerview
+{
+    public class OnItemClickListener<T> : Java.Lang.Object, View.IOnClickListener
+    {
+        public Action<T, View, int> Action;
+
+        public T Model { get; set; }
+
+        public View View { get; set; }
+
+        public int Position { get; set; }
+
+        public void OnClick(View v) => Action?.Invoke(Model, View, Position);
+    }
+}
--- Xamarin.Android.RecyclerView.Presenter/net/kibotu/recyclerview/PresenterViewModel.cs
+++ Xamarin.Android.RecyclerView.Presenter/net/kibotu/recyclerview/PresenterViewModel.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using Android.Views;
-
-namespace net.kibotu.recyclerview
+﻿namespace net.kibotu.recyclerview
 {
     public class PresenterViewModel<T>
     {
         public T Model { get; set; }
 
-        public Action<T, View, int> OnClick { get; set; }
+        public OnItemClickListener<T> OnClick { get; set; }
     }
 }
